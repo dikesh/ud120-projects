7bd81f7a6d6bb0bb6b5f1e5a31053c5f672b7571
dikesh <dikesh@tatvic.com>
2017-03-30 18:25:39 +0530

    Outlier mini project done

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -7,13 +7,25 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
-### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+# read in data dictionary, convert to numpy array
+data_dict = pickle.load(
+    open("../final_project/final_project_dataset.pkl", "r"))
 features = ["salary", "bonus"]
+
+data_dict.pop('TOTAL', None)
 data = featureFormat(data_dict, features)
 
+from pandas import DataFrame
 
-### your code below
+df = DataFrame(data, columns=['salary', 'bonus', ])
+print df.sort_values('salary')
 
+# your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter(salary, bonus)
 
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
