6bcb5d222e31c4bbd3c46271f888a9dd5bd77313
dikesh <dikesh.jariwala@gmail.com>
2017-04-01 17:58:57 +0530

    Feature Scaling done

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -12,10 +12,9 @@ import numpy
 import matplotlib.pyplot as plt
 import sys
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
-
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
@@ -50,10 +49,10 @@ data_dict.pop("TOTAL", 0)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 # For clustering with 3 features
-feature_3 = 'total_payments'
+# feature_3 = 'total_payments'
 poi  = "poi"
 
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2, ]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -62,12 +61,22 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, f3 in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+scaler = MinMaxScaler()
+
+import pandas as pd
+df = pd.DataFrame(finance_features, columns=[feature_1, feature_2,])
+print df.sort_values(feature_1)
+print df.sort_values(feature_2)
+
+finance_features = scaler.fit_transform(finance_features)
+print scaler.scale_
+
 kmeans = KMeans(n_clusters=2)
 kmeans.fit(finance_features)
 pred = kmeans.predict(finance_features)
