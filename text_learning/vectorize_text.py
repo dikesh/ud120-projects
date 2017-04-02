b0f24f932e31837c8b3df30bbca12578b6ac2d33
dikesh <dikesh.jariwala@gmail.com>
2017-04-02 13:20:05 +0530

    Text Learning done

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -42,22 +42,34 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200:
+        if temp_counter < 100 or True:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            parsed_text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            # signature_words = ["sara", "shackleton", "chris", "germani"]
+            # for sw in signature_words:
+            #     parsed_text = parsed_text.replace(sw, '')
+            signature = re.compile(r'(sara|shackleton|chris|germani)', re.I)
+            extracted_text = signature.sub("", parsed_text)
 
             ### append the text to word_data
+            word_data.append(extracted_text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == 'Sara':
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
+
+print word_data[152]
 
 print "emails processed"
 from_sara.close()
@@ -67,9 +79,11 @@ pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
-
-
-
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer.fit_transform(word_data)
 
-
+all_features = vectorizer.get_feature_names()
+print len(all_features)
+print all_features[34597]
